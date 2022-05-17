f20034975856da74348cc8941fbabad9baa2d5ac
Torma Kristóf <tormakristof@tormakristof.eu>
2022-05-17 17:50:38 +0200

    create functionapp resourcegroup

--- azure-resourcegroups.tf
+++ azure-resourcegroups.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "rg-cv-staticwebapp" {
   name     = "rg-cv-staticwebapp"
   location = "westeurope"
 }
+
+resource "azurerm_resource_group" "rg-hanyadikhetvan-functionapp" {
+  name     = "rg-hanyadikhetvan-functionapp"
+  location = "westeurope"
+}
