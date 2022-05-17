f67870fa25a20059d521e080f65d2147f2139dd9
Torma Kristóf <tormakristof@tormakristof.eu>
2022-05-17 19:19:01 +0200

    add start date to budget

--- azure-budget.tf
+++ azure-budget.tf
@@ -7,6 +7,9 @@ resource "azurerm_consumption_budget_subscription" "subscription-budget" {
   amount     = 5
   time_grain = "Monthly"
 
+  time_period {
+    start_date = "2022-05-01T00:00:00Z"
+  }
 
   notification {
     enabled        = true
