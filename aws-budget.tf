43c5c046ec7054c94e3ad6f8bc00058f8f673240
Torma Kristóf <tormakristof@tormakristof.eu>
2022-05-17 18:25:21 +0200

    add aws budget alert

---
+++ aws-budget.tf
@@ -0,0 +1,15 @@
+resource "aws_budgets_budget" "aws-budget" {
+  name              = "budget-monthly"
+  budget_type       = "COST"
+  limit_amount      = "5"
+  limit_unit        = "EUR"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 85
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["tormakristof@tormakristof.eu"]
+  }
+}
