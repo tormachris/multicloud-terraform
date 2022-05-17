06eab4a49030dc09aa0405fcb811901d7c4dda55
Torma Kristóf <tormakristof@tormakristof.eu>
2022-05-17 19:14:15 +0200

    fix subscription reference in azure budget

--- azure-budget.tf
+++ azure-budget.tf
@@ -2,7 +2,7 @@ data "azurerm_subscription" "current-subscription" {}
 
 resource "azurerm_consumption_budget_subscription" "subscription-budget" {
   name            = "subscription-budget"
-  subscription_id = data.azurerm_subscription-subscription.current.id
+  subscription_id = data.azurerm_subscription.current-subscription.id
 
   amount     = 5
   time_grain = "Monthly"
