a44b84f0b4bf915d0ca8315d950ce36f7fc75755
Torma Kristóf <tormakristof@tormakristof.eu>
2022-05-17 19:44:27 +0200

    add function rg

--- azure-resourcegroups.tf
+++ azure-resourcegroups.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "rg-cv-staticwebapp" {
   name     = "rg-cv-staticwebapp"
   location = "westeurope"
 }
+
+resource "azurerm_resource_group" "rg-hanyadikhet-functionapp" {
+  name     = "rg-hanyadikhet-functionapp"
+  location = "westeurope"
+}
